e0f0db27711796bab2c88d22e9e0401d4dd59ad7
Ismavreverse <bgautam.0707@gmail.com>
2018-09-29 21:20:33 +0530

    image serving added in falcon

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import falcon
-
+import mimetypes
+import io
 
 class StaticResource(object):
     def on_get(self, req, resp):
@@ -8,5 +9,15 @@ class StaticResource(object):
         with open('cogvi_web/index.html', 'r') as f:
             resp.body = f.read()
 
+
+class ServeImages(object):
+    def __init__(self, image_store):
+        self._image_store = image_store
+
+    def on_get(self, req, resp, name):
+        resp.content_type = mimetypes.guess_type(name)[0]
+        resp.stream = io.open(self._image_store+'/'+name,'rb')
+
 app = falcon.API()
 app.add_route('/', StaticResource())
+app.add_route('/images/{name}',ServeImages('images'))
